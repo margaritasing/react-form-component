f50ed20197a6a8878c4f912714f2e403e494ea48
margaritasing <libiamfreitesm@gmail.com>
2023-01-02 17:32:16 -0300

    agrego el max count

--- src/component-patterns/components/ProductButton.tsx
+++ src/component-patterns/components/ProductButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useCallback, useContext } from "react"
 import { ProductContext } from "./ProductCard"
 import styles from '../styles/styles.module.css';
 
@@ -10,7 +10,13 @@ export interface Props {
 
 export const ProductButton = ({className, style}: Props) => {
 
-  const { increaseBy, counter } = useContext(ProductContext)
+  const { increaseBy, counter, maxCount } = useContext(ProductContext);
+
+  const isMaxReached = useCallback(
+    () => !!maxCount && counter === maxCount,
+    [counter, maxCount],
+  )
+  
   
   return (
     <div className={ `${styles.buttonsContainer}  ${className}`}
@@ -22,7 +28,10 @@ export const ProductButton = ({className, style}: Props) => {
   
         <div className={styles.countLabel}> { counter }</div>
   
-        <button onClick={() => increaseBy(+1)} className={styles.buttonAdd}> + </button>
+        <button 
+        className={`${styles.buttonAdd} ${isMaxReached() && styles.disabled}`}
+        onClick={() => increaseBy(+1)}
+        > + </button>
   
     </div>
   
